f226b4eb01c052b03c764458c417aa34149c0ed5
NyakundiMauya <n.mauya.nm@gmail.com>
2023-05-19 09:07:52 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,16 @@
-# Your code here!
+def greetProgrammer
+    puts "Hello, programmer!"
+  end
+   def greet(Naureen)
+    puts "Hello, #{Naureen}!"
+  end
+   def greetWithDefault(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+   def add(num1, num2)
+    return num1 + num2
+  end
+   def halve(number)
+    return nil unless number.is_a? Numeric
+    number / 2
+  end
